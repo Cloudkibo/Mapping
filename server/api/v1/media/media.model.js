b42d761bdc7fca0f0f45284206eda5a3993102e2
Baqar <bjafri5@gmail.com>
2018-08-31 16:53:13 +0500

    upload, retrieve, and delete endpoints for media

--- server/api/v1/media/media.model.js
+++ server/api/v1/media/media.model.js
@@ -2,10 +2,9 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 let mediaSchema = new Schema({
-    mediaId: String, 
-    contactId: { type: Schema.ObjectId, ref: 'contacts' }, 
-    groupId: { type: Schema.ObjectId, ref: 'groups' }, 
-    mediaType: String,
+  mediaId: String,
+  mediaType: String,
+  url: String
 })
 
 module.exports = mongoose.model('media', mediaSchema)
